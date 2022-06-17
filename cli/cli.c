5d17f9d26734be33a831dbaa5422794e898446aa
zhngqi_ubuntu <zhengquan@micronet.ltd>
2022-06-17 14:14:51 +0800

    修改main()函数位置

--- cli/cli.c
+++ cli/cli.c
@@ -7,6 +7,48 @@ int _sock = 0;
 int inotfy_fd = 0;
 struct dir_link *head_link = NULL;  //目录监控头指针
 
+void event_printf(struct inotify_event *event);
+
+//主程序
+int main(void)
+{   
+    //自定义信号
+    inotfy_fd = inotify_init();
+    if (inotfy_fd == -1) {
+        perror("inotify init error");
+        exit(-1);
+    }
+
+    if ((_sock = init_tcp()) == -1 ) {
+        perror("sock error");
+        exit (-1);
+    }
+
+    signal(SIGUSR1, signal_do);
+
+    struct dir_link *path_link = (struct dir_link *)malloc(sizeof(struct dir_link));//头节点
+    (void)memset(path_link, 0, sizeof(struct dir_link));
+    head_link = path_link;//指向首元节点
+    printf("\nwaiting for first synchronization complete............. \n");
+    first_event(inotfy_fd, SYNC_PATH, 1, head_link, _sock);
+    printf("\nfirst ok\n");
+    while (1) {
+        int dir_read = read(inotfy_fd, readbuf, sizeof(readbuf));
+        for (char *p = readbuf ; p < (readbuf+dir_read);) {   
+            struct inotify_event *event;
+            event = (struct inotify_event*)p;
+            p+= sizeof(struct inotify_event) + event->len;
+            
+            event_printf(event);
+            do_event(inotfy_fd, event, head_link, _event_P);
+        } 
+        memset(readbuf, 0, sizeof(readbuf));
+    }
+    return 0;
+}
+
+
+
 void event_printf(struct inotify_event *event)
 {
             /*
@@ -62,45 +104,4 @@ void event_printf(struct inotify_event *event)
             printf("event-->wd:%d\n",event -> wd);
             printf("---------------------------------\n");
 }
-
-//主程序
-int main(void)
-{   
-    //自定义信号
-    inotfy_fd = inotify_init();
-    if (inotfy_fd == -1) {
-        perror("inotify init error");
-        exit(-1);
-    }
-
-    if ((_sock = init_tcp()) == -1 ) {
-        perror("sock error");
-        exit (-1);
-    }
-
-    signal(SIGUSR1, signal_do);
-
-    struct dir_link *path_link = (struct dir_link *)malloc(sizeof(struct dir_link));//头节点
-    (void)memset(path_link, 0, sizeof(struct dir_link));
-    head_link = path_link;//指向首元节点
-    printf("\nwaiting for first synchronization complete............. \n");
-    first_event(inotfy_fd, SYNC_PATH, 1, head_link, _sock);
-    printf("\nfirst ok\n");
-    while (1) {
-        int dir_read = read(inotfy_fd, readbuf, sizeof(readbuf));
-        for (char *p = readbuf ; p < (readbuf+dir_read);) {   
-            struct inotify_event *event;
-            event = (struct inotify_event*)p;
-            p+= sizeof(struct inotify_event) + event->len;
-            
-            event_printf(event);
-            do_event(inotfy_fd, event, head_link, _event_P);
-        } 
-        memset(readbuf, 0, sizeof(readbuf));
-    }
-    return 0;
-}
-
-
-
 
